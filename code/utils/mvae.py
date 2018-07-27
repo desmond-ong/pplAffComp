7b5f859f293c73f2601a7b8a7a4a86fd0b9b5e8a
Tan Zong Xuan <tanqazx@gmail.com>
2018-07-27 18:31:26 +0800

    Updated with fixes by Desmond to the Jupyter Notebook.

--- code/utils/mvae.py
+++ code/utils/mvae.py
@@ -5,6 +5,7 @@ from torch.nn import functional as F
 
 import pyro
 import pyro.distributions as dist
+import pyro.poutine as poutine
 
 from networks import ImageEncoder, ImageDecoder, Encoder, Decoder
 
@@ -38,24 +39,39 @@ class MVAE(nn.Module):
     in a future version will make them be inputs into the constructor
     """
     def __init__(self, z_dim, img_width, embed_dim,
-                 rating_dim, outcome_dim, use_cuda=False):
+                 rating_dim, outcome_dim, use_cuda=False, lambdas={}):
         super(MVAE, self).__init__()
         self.z_dim = z_dim
+
         self.img_width = img_width
         self.image_encoder = ImageEncoder(z_dim)
         self.image_decoder = ImageDecoder(z_dim)
+
         self.embed_dim = embed_dim
         self.word_encoder = Encoder(z_dim, embed_dim)
         self.word_decoder = Decoder(z_dim, embed_dim)
+
         self.rating_dim = rating_dim
         self.rating_encoder = Encoder(z_dim, rating_dim)
         self.rating_decoder = Decoder(z_dim, rating_dim)
+
         self.outcome_dim = outcome_dim
         self.outcome_encoder = Encoder(z_dim, outcome_dim)
         self.outcome_decoder = Decoder(z_dim, outcome_dim)
+
         self.experts = ProductOfExperts()
         self.use_cuda = use_cuda
         
+        self.lambdas = lambdas
+        if "image" not in self.lambdas:
+            self.lambdas["image"] = 1.0
+        if "word" not in self.lambdas:
+            self.lambdas["word"] = 1.0
+        if "rating" not in self.lambdas:
+            self.lambdas["rating"] = 50.0
+         if "outcome" not in self.lambdas:
+            self.lambdas["outcome"] = 100.0
+       
         # using GPUs for faster training of the networks
         if self.use_cuda:
             self.cuda()
@@ -68,39 +84,16 @@ class MVAE(nn.Module):
             self.outcome_encoder.cuda()
             self.outcome_decoder.cuda()
             self.experts.cuda()
-
-    def prior_expert(self, size, use_cuda=False):
-         """Universal prior expert. Here we use a spherical
-         Gaussian: N(0, 1).
-         @param size: integer
-                      dimensionality of Gaussian
-         @param use_cuda: boolean [default: False]
-                          cast CUDA on variables
-         """
-         mu = Variable(torch.zeros(size))
-         logvar = Variable(torch.log(torch.ones(size)))
-         if use_cuda:
-             mu, logvar = mu.cuda(), logvar.cuda()
-         return mu, logvar
             
-    def reparametrize(self, mu, logvar):
-        if self.training:
-            std = logvar.mul(0.5).exp_()
-            eps = Variable(std.data.new(std.size()).normal_())
-            return eps.mul(std).add_(mu)
-        else:  # return mean during inference
-            return mu
-
     def forward(self, word=None, image=None, rating=None, outcome=None):
-        mu, logvar  = self.infer(word, image, rating, outcome)
-        # reparametrization trick to sample
-        z  = self.reparametrize(mu, logvar)
-        # reconstruct inputs based on that gaussian
+        z_loc, z_scale  = self.infer(word, image, rating, outcome)
+        z = pyro.sample("latent", dist.Normal(z_loc, z_scale).independent(1))
         word_recon = self.word_decoder(z)
         image_recon = self.image_decoder(z)
         rating_recon = self.rating_decoder(z)
         outcome_recon = self.outcome_decoder(z)
-        return word_recon, image_recon, rating_recon, outcome_recon, mu, logvar
+        return (word_recon, image_recon, rating_recon, outcome_recon,
+                z_loc, z_scale)
 
     def infer(self, word=None, image=None, rating=None, outcome=None):
         if word is not None:
@@ -114,34 +107,38 @@ class MVAE(nn.Module):
 
         batch_size = 1
 
-        # initialize the universal prior expert
-        mu, logvar = self.prior_expert((1, batch_size, self.z_dim),
-                                       use_cuda=self.use_cuda)
+        # initialize the universal prior
+        z_loc = torch.zeros(torch.Size((1, batch_size, self.z_dim))) + 0.5
+        z_scale = torch.ones(torch.Size((1, batch_size, self.z_dim))) * 0.1
+        if self.use_cuda:
+            z_loc, z_scale = z_loc.cuda(), z_scale.cuda()
+        
         if word is not None:
-            word_mu, word_logvar = self.word_encoder(word)
-            mu = torch.cat((mu, word_mu.unsqueeze(0)), dim=0)
-            logvar = torch.cat((logvar, word_logvar.unsqueeze(0)), dim=0)
+            word_z_loc, word_z_scale = self.word_encoder(word)
+            z_loc = torch.cat((z_loc, word_z_loc.unsqueeze(0)), dim=0)
+            z_scale = torch.cat((z_scale, word_z_scale.unsqueeze(0)), dim=0)
         
         if image is not None:
-            image_mu, image_logvar = self.image_encoder(image)
-            mu = torch.cat((mu, image_mu.unsqueeze(0)), dim=0)
-            logvar = torch.cat((logvar, image_logvar.unsqueeze(0)), dim=0)
+            image_z_loc, image_z_scale = self.image_encoder(image)
+            z_loc = torch.cat((z_loc, image_z_loc.unsqueeze(0)), dim=0)
+            z_scale = torch.cat((z_scale, image_z_scale.unsqueeze(0)), dim=0)
 
         if rating is not None:
-            rating_mu, rating_logvar = self.rating_encoder(rating)
-            mu = torch.cat((mu, rating_mu.unsqueeze(0)), dim=0)
-            logvar = torch.cat((logvar, rating_logvar.unsqueeze(0)), dim=0)
+            rating_z_loc, rating_z_scale = self.rating_encoder(rating)
+            z_loc = torch.cat((z_loc, rating_z_loc.unsqueeze(0)), dim=0)
+            z_scale = torch.cat((z_scale, rating_z_scale.unsqueeze(0)), dim=0)
 
         if outcome is not None:
-            outcome_mu, outcome_logvar = self.outcome_encoder(outcome)
-            mu     = torch.cat((mu, outcome_mu.unsqueeze(0)), dim=0)
-            logvar = torch.cat((logvar, outcome_logvar.unsqueeze(0)), dim=0)
+            outcome_z_loc, outcome_z_scale = self.outcome_encoder(outcome)
+            z_loc = torch.cat((z_loc, outcome_z_loc.unsqueeze(0)), dim=0)
+            z_scale = torch.cat((z_scale, outcome_z_scale.unsqueeze(0)), dim=0)
 
         # product of experts to combine gaussians
-        mu, logvar = self.experts(mu, logvar)
-        return mu, logvar
+        z_loc, z_scale = self.experts(z_loc, z_scale)
+        return z_loc, z_scale
     
-    def model(self, words=None, images=None, ratings=None, outcomes=None):
+    def model(self, words=None, images=None, ratings=None, outcomes=None,
+              annealing_beta=1.0):
         # register this pytorch module and all of its sub-modules with pyro
         pyro.module("mvae", self)
         
@@ -156,62 +153,58 @@ class MVAE(nn.Module):
             batch_size = outcomes.size(0)
         
         with pyro.iarange("data", batch_size):
-            if outcomes is not None:
-                # sample from outcome prior, compute p(z|outcome)
-                outcome_prior_loc = torch.zeros(\
-                        torch.Size((batch_size, self.outcome_dim)))
-                outcome_prior_scale = torch.ones(\
-                        torch.Size((batch_size, self.outcome_dim)))
-                if self.use_cuda:
-                    outcome_prior_loc = outcome_prior_loc.cuda()
-                    outcome_prior_scale = outcome_prior_scale.cuda()
-
-                pyro.sample("obs_outcome",
-                            dist.Normal(outcome_prior_loc,
-                                        outcome_prior_scale).independent(1),
-                            obs=outcomes.reshape(-1, self.outcome_dim))
-                
-                z_loc, z_scale = self.outcome_encoder.forward(outcomes)
-            else:
-                # setup hyperparameters for prior p(z)
-                z_loc = torch.zeros(torch.Size((batch_size, self.z_dim)))
-                z_scale = torch.ones(torch.Size((batch_size, self.z_dim)))
-                if self.use_cuda:
-                    z_loc, z_scale = z_loc.cuda(), z_scale.cuda()
+            z_loc = torch.zeros(torch.Size((1, batch_size, self.z_dim))) + 0.5
+            z_scale = torch.ones(torch.Size((1, batch_size, self.z_dim))) * 0.1
+            if self.use_cuda:
+                z_loc, z_scale = z_loc.cuda(), z_scale.cuda()
             
             # sample from prior
             # (value will be sampled by guide when computing the ELBO)
-            z = pyro.sample("latent",
-                            dist.Normal(z_loc, z_scale).independent(1))
-            # decode the latent code z
+            z_dist = dist.Normal(z_loc, z_scale).independent(1)
+            with poutine.scale(scale=annealing_beta):
+                z = pyro.sample("latent", z_dist)
 
+            # decode the latent code z
+            
             word_loc, word_scale = self.word_decoder.forward(z)
+            word_dist = dist.Normal(word_loc, word_scale).independent(1)
             # score against actual words
             if words is not None:
-                pyro.sample("obs_word",
-                            dist.Normal(word_loc, word_scale).independent(1), 
-                            obs=words.reshape(-1, self.embed_dim))
+                with poutine.scale(scale=self.lambdas["word"]):
+                    pyro.sample("obs_word", word_dist
+                                obs=words.reshape(-1, self.embed_dim))
             
             img_loc = self.image_decoder.forward(z)
+            img_dist = dist.Bernoulli(img_loc).independent(1)
             # score against actual images
             if images is not None:
-                pyro.sample("obs_img",
-                            dist.Bernoulli(img_loc).independent(1), 
-                            obs=images.reshape(-1, 3,
-                                               self.img_width, self.img_width))
+                with poutine.scale(scale=self.lambdas["image"]):
+                    pyro.sample("obs_img", img_dist,
+                                obs=images.reshape(-1, 3, self.img_width,
+                                                   self.img_width))
             
             rating_loc, rating_scale = self.rating_decoder.forward(z)
+            rating_dist = dist.Normal(rating_loc, rating_scale).independent(1)
             # score against actual ratings
             if ratings is not None:
-                pyro.sample("obs_rating",
-                            dist.Normal(rating_loc,
-                                        rating_scale).independent(1), 
-                            obs=ratings.reshape(-1, self.rating_dim))
+                with poutine.scale(scale=self.lambdas["rating"]):
+                    pyro.sample("obs_rating", rating_dist
+                                obs=ratings.reshape(-1, self.rating_dim))
 
+            outcome_loc, outcome_scale = self.outcome_decoder.forward(z)
+            outcome_dist = dist.Normal(outcome_loc,
+                                       outcome_scale).independent(1)
+            # score against actual outcomes
+            if outcomes is not None:
+                with poutine.scale(scale=self.lambdas["outcome"]):
+                    pyro.sample("obs_outcome", outcome_dist,
+                                obs=outcomes.reshape(-1, self.outcome_dim))
+                
             # return the loc so we can visualize it later
-            return word_loc, img_loc, rating_loc
+            return word_loc, img_loc, rating_loc, outcome_loc
     
-    def guide(self, words=None, images=None, ratings=None, outcomes=None):
+    def guide(self, words=None, images=None, ratings=None, outcomes=None,
+              annealing_beta=1.0):
         # register this pytorch module and all of its sub-modules with pyro
         pyro.module("mvae", self)
         
@@ -266,5 +259,7 @@ class MVAE(nn.Module):
             
             z_loc, z_scale = self.experts(z_loc, z_scale)
             # sample the latent z
-            pyro.sample("latent", dist.Normal(z_loc, z_scale).independent(1))
+            with poutine.scale(scale=annealing_beta):
+                pyro.sample("latent",
+                            dist.Normal(z_loc, z_scale).independent(1))
     
